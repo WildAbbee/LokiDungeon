50e198a709a4098f00f94e34997627f1663afb22
WildAbbee <76451684+WildAbbee@users.noreply.github.com>
2022-07-22 20:00:58 +0100

    Add files via upload

---
+++ LokiDungeon/src/me/WildAbbee/EscapeEvent/ResetCommand.java
@@ -0,0 +1,45 @@
+package me.WildAbbee.EscapeEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ResetCommand implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length < 1) {
+			sender.sendMessage("/lokireset <new goal amount>");
+			return false;
+		}
+
+		int amount = -1;
+		try {
+			amount = Integer.parseInt(args[0]);
+		} catch (NumberFormatException e) {
+			sender.sendMessage(args[0] + " isn't an integer.");
+		}
+
+		if (amount < 1) {
+			sender.sendMessage(args[0] + " must be greater than 0.");
+			return false;
+		}
+
+		EscapeEvent.inst.amount = amount;
+		EscapeEvent.inst.checkpoints.clear();
+		EscapeEvent.inst.eggs.clear();
+		EscapeEvent.inst.lastMessages.clear();
+		EscapeEvent.inst.deaths.clear();
+		
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			for (Player p2 : Bukkit.getOnlinePlayers()) {
+				p.showPlayer(p2);
+			}
+		}
+		
+		Bukkit.broadcastMessage("Escape event reset by " + sender.getName() + " (goal: " + amount + ")");
+		
+		return true;
+	}
+}
